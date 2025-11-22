acff1c2f938da0cd3a2081a70569406e3a9801c6
techai342 <tiktokusr4400@gmail.com>
2025-11-22 19:55:12 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Features from './components/Features'
-import Footer from './components/Footer'
-import './index.css'
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Categories from './components/Categories';
+import FeaturedProducts from './components/FeaturedProducts';
+import ProductShowcase from './components/ProductShowcase';
+import Testimonials from './components/Testimonials';
+import Newsletter from './components/Newsletter';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main>
-        <Hero />
-        <Features />
-        {/* Add more sections here */}
-      </main>
+      <Hero />
+      <Categories />
+      <FeaturedProducts />
+      <ProductShowcase />
+      <Testimonials />
+      <Newsletter />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
